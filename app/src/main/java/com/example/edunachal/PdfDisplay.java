c8ac3c115f7042e93299ce9d65f7ccb6bf29de6a
kaustubh-vats <kaustubhvats08@gmail.com>
2020-11-29 17:10:59 +0530

    Added Pdf view

--- app/src/main/java/com/example/edunachal/PdfDisplay.java
+++ app/src/main/java/com/example/edunachal/PdfDisplay.java
@@ -11,7 +11,9 @@ import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 import com.github.barteksc.pdfviewer.listener.OnRenderListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -62,7 +64,18 @@ public class PdfDisplay extends AppCompatActivity {
         @Override
         protected void onPostExecute(InputStream inputStream) {
             super.onPostExecute(inputStream);
-            pdfView.fromStream(inputStream).load();
+            pdfView.fromStream(inputStream).onRender(new OnRenderListener() {
+                @Override
+                public void onInitiallyRendered(int nbPages, float pageWidth, float pageHeight) {
+                    pdfView.fitToWidth();
+                }
+            }).onLoad(new OnLoadCompleteListener() {
+                @Override
+                public void loadComplete(int nbPages) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }).scrollHandle(new DefaultScrollHandle(PdfDisplay.this))
+                    .enableSwipe(true).defaultPage(0).load();
         }
     }
 }
